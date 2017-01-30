47403032627711f44195895d1c62bec987e9ff71
darebe <brzozajo@yahoo.com>
2017-01-30 22:05:56 +0100

    Simple read and plot to output

--- main.cpp
+++ main.cpp
@@ -1,27 +1,48 @@
 #include <iostream>
 #include <memory>
+#include <fstream>
 
 using namespace std;
 
 
-class bar;
+//class bar;
 
-class foo
-{
-public:
-    foo(const std::shared_ptr<bar>& b)
-        : forward_reference{b}  { }
+//class foo
+//{
+//public:
+//    foo(const std::shared_ptr<bar>& b)
+//        : forward_reference{b}  { }
 
-private:
-    std::shared_ptr<bar> forward_reference;
-};
+//private:
+//    std::shared_ptr<bar> forward_reference;
+//};
 
 
 
 int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    cout << "Hello Easter Bunny HQ!" << endl;
+
+    string x;
+    int count_position;
+    count_position = 0;
+
+
+    ifstream myPositionsByText;
+    myPositionsByText.open("01.txt");
+    char outputSteps;
+    if(myPositionsByText.is_open())
+    {
+        while(!myPositionsByText.eof())
+        {
+            myPositionsByText >> outputSteps;
+            cout << outputSteps;
+        }
+    }
+
+//    fstream fcin;
+
     return 0;
-    unsigned a;
-    unsigned b;
+//    unsigned a;
+//    unsigned b;
 }
